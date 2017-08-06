7d8e0889b3f693c6428b6c508db15ab513a6b6d8
theArtist24 <xu.1607870@studenti.uniroma1.it>
2017-08-06 17:01:17 +0200

    h

---
+++ perceptron.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.linear_model as sk
+from matplotlib.colors import ListedColormap
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
+#print(df)
+
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+#print(X)
+# plot data
+plt.scatter(X[:50, 0], X[:50, 1],
+            color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+            color='blue', marker='x', label='versicolor')
+
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+#plt.savefig('./images/02_06.png', dpi=300)
+plt.show()
+
+
+
+#Training the perceptron
+ppn = sk.Perceptron(alpha=0.1, n_iter=10)
+
+ppn.fit(X, y)
+
+#plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+#plt.xlabel('Epochs')
+#plt.ylabel('Number of updates')
+
+#plt.tight_layout()
+# plt.savefig('./perceptron_1.png', dpi=300)
+#plt.show()
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
+
+
+plot_decision_regions(X, y, classifier=ppn)
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+# plt.savefig('./perceptron_2.png', dpi=300)
+plt.show()
+
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+
+ada1 = sk.SGDClassifier(n_iter=10, alpha=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.loss) + 1), np.log10(ada1.loss), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = sk.SGDClassifier(n_iter=10, alpha=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.tight_layout()
+# plt.savefig('./adaline_1.png', dpi=300)
+plt.show()
